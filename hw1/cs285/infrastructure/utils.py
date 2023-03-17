5a4da1db144d7f333d2790641924e3ab003f1803
huijian222 <laijinbang2009@gmail.com>
2023-03-17 20:19:22 +0800

    hw1 模仿学习 dagger

--- hw1/cs285/infrastructure/utils.py
+++ hw1/cs285/infrastructure/utils.py
@@ -42,7 +42,7 @@ def sample_trajectory(env, policy, max_path_length, render=False):
 
         # TODO end the rollout if the rollout ended
         # HINT: rollout can end due to done, or due to max_path_length
-        rollout_done = 1 if done or steps == max_path_length else 0  # HINT: this is either 0 or 1
+        rollout_done = 1 if done or steps >= max_path_length else 0  # HINT: this is either 0 or 1
         terminals.append(rollout_done)
 
         if rollout_done:
